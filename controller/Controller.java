555e69cd3c49db019a91d374510e6b9794ee8d5d
FourEyedGuy <ninjayanyan@gmail.com>
2015-12-26 23:46:24 +0100

    Revert "Revert "Petite clean du code""
    This reverts commit 0020b4fb34eba4ee00fa03331ddf41ad0a324adb.

--- controller/Controller.java
+++ controller/Controller.java
@@ -17,10 +17,6 @@ public class Controller extends AbstractController{
 		}
 		else
 			playControl();
-		
-		//System.out.println(gameMngr.toString());
-		//System.out.println("(pawnLine, pawnColumn, destLine, destColumn) - apres control :\n(" + pawnLine + "," + pawnColumn + "," + destLine + "," + destColumn +")");
-		//System.out.println();
 	}
 
 	@Override
@@ -50,9 +46,6 @@ public class Controller extends AbstractController{
 				gameMngr.setInitPhase(false);
 			}
 		}
-		
-		//System.out.println("(pawnLine, pawnColumn, destLine, destColumn) - init control :\n(" + pawnLine + "," + pawnColumn + "," + destLine + "," + destColumn +")");
-		
 		pawnLine = -1;
 		pawnColumn = -1;
 	}
@@ -81,16 +74,12 @@ public class Controller extends AbstractController{
 			if(!gameEnded()) gameMngr.switchTurn();
 		}
 		
-		//System.out.println("(pawnLine, pawnColumn, destLine, destColumn) - play control :\n(" + pawnLine + "," + pawnColumn + "," + destLine + "," + destColumn +")");
-		
 		if(destLine >= 0 && destColumn >= 0){
 			pawnLine = -1;
 			pawnColumn = -1;
 			destLine = -1;
 			destColumn = -1;
 		}
-		
-		
 	}
 	
 	private boolean isAValidMove(){
